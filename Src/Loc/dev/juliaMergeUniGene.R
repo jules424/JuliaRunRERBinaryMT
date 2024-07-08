044c828ac4d08393d9eab0ed510f5334d3760001
jules424 <jma424@lehigh.edu>
2024-07-08 16:30:57 -0400

    dental push

--- Src/Loc/dev/juliaMergeUniGene.R
+++ Src/Loc/dev/juliaMergeUniGene.R
@@ -2,6 +2,9 @@
   # 26 FEB 2024
   # this will merge unicorn/gene/pheno csv files with the RER output file
 
+library(readr)
+library(ggplot2)
+library(grafify)
 # raw csv RER correlation file 
 acuteLophsCorrelationFile = read.csv("Output/AcuteLophsFull/AcuteLophsFullCorrelationFile.csv")
 
@@ -12,16 +15,13 @@ geneCorrelationFile = read.table("Data/unicornOverlap.tsv")
 toothPhenoFile = read.csv("Data/toothCandidateGenes.csv")
 
 names(acuteLophsCorrelationFile)[1] = "unicornsID"
-mergedData = merge(acuteLophsCorrelationFile, geneCorrelationFile, by.x= "unicornsID", by.y= "V8", all.x  = T)
-mergedAcuteLophsWithPheno = merge(mergedData, toothPhenoFile, by.x= "V4", by.y= "ENSG.ID", all.x  = T)
+# mergedData = merge(acuteLophsCorrelationFile, geneCorrelationFile, by.x= "unicornsID", by.y= "V8", all.x  = T)
+# mergedAcuteLophsWithPheno = merge(mergedData, toothPhenoFile, by.x= "V4", by.y= "ENSG.ID", all.x  = T)
+# write.csv(mergedAcuteLophsWithPheno, "Data/mergedAcuteLophs.csv")
+mergedAcuteLophsPhenoShortened = read.csv("Data/mergedAcuteLophsShortened.csv")
 
 # VIOLIN PLOT FOR ACUTE LOPHS
-library(readr)
-library(ggplot2)
-library(grafify)
-#Grafify requires the following non-exported function from ggplot2:
-#(obtained from here: https://github.com/tidyverse/ggplot2/blob/master/R/guides-axis.r)
-guide_axis <- function(title = waiver(), check.overlap = FALSE, angle = NULL, n.dodge = 1,
+guide_axis <- function(title = waiver(), check.overlap = TRUE, angle = NULL, n.dodge = 1,
                        order = 0, position = waiver()) {
   structure(
     list(
@@ -45,27 +45,20 @@ guide_axis <- function(title = waiver(), check.overlap = FALSE, angle = NULL, n.
   )
 }
 
-plot_scatterviolin(data=mergenew, xcol=Type, ycol=AUC, 
+mergedAcuteLophsPhenoShortened$PHENOTYPE <- factor(mergedAcuteLophsPhenoShortened$PHENOTYPE,
+                          levels=c("Bud arrest", "Initiation arrest", "Bell arrest", "Abnormal ameloblast",
+                                   "No enamel", "Less enamel", "Hyperdontia", "Ectopic elements", "Root malformation", "Other", "Control"))
+plot_scatterviolin(data=mergedAcuteLophsPhenoShortened, 
+                   xcol=PHENOTYPE,ycol=Rho, 
                    symsize=3,
                    ColPal = "safe")+
   theme_classic()+
   theme(legend.position="none")+
-  theme(text = element_text(size=20))
-
-
-ggsave("DietGeneViolin.pdf",width=12,height=7)
+  theme(text = element_text(size=8)) +
+  geom_point(shape = NA, color = NA)
 
 
 
-mergedAcuteLophsWithPheno$Rho <- as.factor(mergedAcuteLophsWithPheno$Rho)
-head(mergedAcuteLophsWithPheno)
-library(ggplot2)
-# Basic violin plot
-violinAcuteLophs <- ggplot(mergedAcuteLophsWithPheno, aes(x=Rho, y=GENE)) + 
-  geom_violin()
-violinAcuteLophs
-# Rotate the violin plot
-p + coord_flip()
-# Set trim argument to FALSE
-ggplot(ToothGrowth, aes(x=dose, y=len)) + 
-  geom_violin(trim=FALSE)
+ggsave("Output/ToothViolinUpdate.pdf",width=100,height=50)
+    plot(ToothViolinUpdate.pdf)
+    
